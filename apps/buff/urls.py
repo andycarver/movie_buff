bbe358ab81cccc1962735d3e1fc960be0c2fc295
Ben Swanson <bentswanson@gmail.com>
2016-11-14 14:55:48 -0800

    edited urls

--- apps/buff/urls.py
+++ apps/buff/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^add_actor$', views.add_actor),
-    url(r'^show_actor$', views.show_actor),
-    url(r'^add_movie$', views.add_movie),
-    url(r'^update_actor$', views.update_actor),
+    # url(r'^show_actor$', views.show_actor),
+    # url(r'^add_movie$', views.add_movie),
+    # url(r'^update_actor$', views.update_actor),
 ]
